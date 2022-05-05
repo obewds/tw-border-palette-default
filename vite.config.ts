67f151a3ba1d2d6ee3c9755c783adde9ced5db65
OBE (Online Business Engineering) <33532265+oberocks@users.noreply.github.com>
2022-05-04 21:01:04 -0400

    updated docs and capitalization of umd/iife exported name

--- vite.config.ts
+++ vite.config.ts
@@ -26,7 +26,7 @@ export default defineConfig({
     outDir: './dist',
     lib: {
       entry: resolve(__dirname, 'src/index.ts'),
-      name: 'TwBorderPaletteDefault',
+      name: 'twBorderPaletteDefault',
       formats: ['es','cjs','umd','iife'],
       fileName: (format: string) => `tw-border-palette-default.${format}.js`,
     },
