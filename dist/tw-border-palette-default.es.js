10d7befadee0b2bb1cfecfc26567320da6f438b2
OBE (Online Business Engineering) <33532265+oberocks@users.noreply.github.com>
2022-08-30 07:50:09 -0400

    1.2.0: updated deps, smaller payload from vite optimizations, and switch to automated publishing

--- dist/tw-border-palette-default.es.js
+++ dist/tw-border-palette-default.es.js
@@ -1,13 +1,15 @@
-var twBorderPaletteDefault = {
-  "default": {
+const r = {
+  default: {
     colors: {
       "": "",
-      "default": "border-gray-200 dark:border-gray-600",
-      "error": "border-rose-600 dark:border-rose-300",
-      "primary": "border-blue-600 dark:border-blue-300",
-      "secondary": "border-violet-600 dark:border-violet-300",
-      "success": "border-green-600 dark:border-green-300"
+      default: "border-gray-200 dark:border-gray-600",
+      error: "border-rose-600 dark:border-rose-300",
+      primary: "border-blue-600 dark:border-blue-300",
+      secondary: "border-violet-600 dark:border-violet-300",
+      success: "border-green-600 dark:border-green-300"
     }
   }
 };
-export { twBorderPaletteDefault };
+export {
+  r as twBorderPaletteDefault
+};
